53f45a7358831c150e3dd7dbef6d9eba3714e554
DeepHobbying <40418415+DeepHobbying@users.noreply.github.com>
2018-07-02 11:33:53 -0700

    Add files via upload

---
+++ LT4_ArithmeticOperators.py
@@ -0,0 +1,40 @@
+#Arithmetic operators
+
+x = 17
+y = 7
+
+
+#Addition
+
+print("x + y is equal to",x+y)
+
+
+#Subtraction
+
+print("x - y is equal to",x-y)
+
+
+#Multiplication
+
+print("x * y is equal to",x*y)
+
+
+#Division
+
+print("x / y is equal to",x/y)
+
+
+#Floor division
+
+print("x // y is equal to",x//y)
+
+
+#Modulus division
+
+print("x % y is equal to",x%y)
+
+#Exponent
+
+print("x to the power 3 is equal to",x**3)
+
+
---
+++ LT4_AssignmentOperators.py
@@ -0,0 +1,16 @@
+#Assignment Operators
+x = 5
+
+print("x = x+1")
+x = x +1
+print(x)
+
+x = 5
+print("x += 1")
+x += 1
+print(x)
+
+x = 5
+print("x *= 1")
+x *= 3
+print(x)
