b80a896a106740e69059ddcab9e25be72895256a
DeepHobbying <40418415+DeepHobbying@users.noreply.github.com>
2018-07-04 12:44:33 -0700

    Add files via upload

---
+++ LT5_ComparisionLogicalBitwiseIdentityMembershipOperators.py
@@ -0,0 +1,84 @@
+'''
+#Comparison operators
+
+x = 5
+y = 8
+
+#Greater than
+
+print("if x is",x, "and y is",y, "then x>y will be",x>y)
+
+#Less than
+
+print("if x is",x, "and y is",y, "then x<y will be",x<y)
+
+#Equal to
+
+print("if x is",x, "and y is",y, "then x==y will be",x==y)
+
+#Not equal to
+
+print("if x is",x, "and y is",y, "then x!=y will be",x!=y)
+
+#Greater than or equal to
+
+print("if x is",x, "and y is",y, "then x>=y will be",x>=y)
+
+#Less than or equal to 
+
+print("if x is",x, "and y is",y, "then x<=y will be",x<=y)
+
+
+
+#Logical operators
+
+x = True
+y = False
+
+#and
+print("if x is",x, "and y is",y, "then x and y will be",x and y)
+#or
+print("if x is",x, "and y is",y, "then x or y will be",x or y)
+#not
+print("if x is",x, "then not x will be",not x)
+
+
+
+
+#Bitwise Operators
+
+x = 7
+y = 5
+
+#Bitwise AND (&)
+
+print("if x is",x, "and y is",y, "then x & y will be",x & y)
+
+#Bitwise OR (|)
+
+print("if x is",x, "and y is",y, "then x | y will be",x | y)
+
+#Bitwise XOR (^)
+
+print("if x is",x, "and y is",y, "then x ^ y will be",x ^ y)
+
+#Bitwise right shift (>>)
+
+print("if x is",x, "then 2 digit right shift of x will be", x>>2)
+
+#Bitwise left shift (<<)
+
+print("if x is",x, "then n 2 digit left shif of x will be",x<<2)
+
+'''
+
+#Identity operators
+x = 8
+
+#is
+
+print("x is 8:", x is 8)
+
+#is not
+
+print("x is not 8:", x is not 8)
