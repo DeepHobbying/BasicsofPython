79d146482ec16c59ba784cf2870690b5819ee33e
DeepHobbying <40418415+DeepHobbying@users.noreply.github.com>
2018-07-28 06:47:12 -0700

    Add files via upload

---
+++ LT11_Function.py
@@ -0,0 +1,55 @@
+#Getting help
+
+
+#help()
+
+
+#Simple function
+
+def myfun ():
+    print("this is my first function")
+
+myfun()
+
+#Function name
+
+##def def():
+##    print("dummy")
+
+
+
+
+#function argument(s)
+
+
+def add(num1,num2):
+    item=num1+num2
+
+add(2,3)
+
+
+
+
+#function that return
+
+
+def add(num1,num2):
+    item=num1+num2
+    return(item)
+
+print(add(2,3))
+
+
+
+#function with default argument(s)
+
+def add(num1,num2=7):
+    item=num1+num2
+    return(item)
+
+print(add(2))
+
+
+
+
+
